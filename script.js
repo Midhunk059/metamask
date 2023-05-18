1e46df34dc1dd4c53d9cfa45b4f29ceeffb6b3b1
Midhunk059 <midhunk059@gmail.com>
2023-05-18 14:03:31 +0530

    Commit 2

--- script.js
+++ script.js
@@ -2,40 +2,32 @@ function dark() {
 
     var element = document.body;
     element.classList.toggle("dark-mode");
-   
+    
+    var bg11=document.getElementById("Bg11");
+    var logos = document.getElementById("dLogo");
+    var navlink=document.getElementsByClassName("nav-link");
 
     var image = document.getElementById("light");
-    if (image.src.match("dark.png")) {
+    
+    if (image.src.match("./images/dark.png")) {
         image.src = "./images/light.png";
-    }
-    else {
-        image.src = "./images/dark.png";
-    }
-    var logos = document.getElementById("dLogo");
-    var bg11=document.getElementById("Bg11");
-    var navbar=document.getElementById("navbar");
-    var navlink=document.getElementsByClassName("nav-link");
-    for(let i=0;i<navlink.length;i++){
-        navlink[i].style.color='white';
-   }
-   
-        
-    if (image.src.match("dark.png")) {
-        logos.src = "./images/darklogo.png";
-        bg11.style.backgroundColor='#3c444b';
-        navbar.style.backgroundColor='black';
-        navlink[i].style.color='white';
-        
-    }
-    else{
         bg11.style.backgroundColor='#e9ebee'
         logos.src = "./images/metamask.png";
         navbar.style.backgroundColor='white';
         for(let i=0;i<navlink.length;i++){
-            navlink[i].style.color='black';
-       }
-       
+        navlink[i].style.color='black';
+       }        
     }
+    else {
+        image.src = "./images/dark.png";
+        logos.src = "./images/darklogo.png";
+        bg11.style.backgroundColor='#3c444b';
+        navbar.style.backgroundColor='black';
+        for(let i=0;i<navlink.length;i++){
+        navlink[i].style.color='White';
+       }  
+    }
+    
 }
 
 
